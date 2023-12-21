67879e97b2fa9a457f2d82b5428b5bef0b80330c
Olzhas <olzhasabukhan2003@gmail.com>
2023-12-21 14:11:49 +0600

    Добавьте файлы проекта.

---
+++ examproject/ContactBook.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+// Класс Contact представляет сущность контакта с основными свойствами.
+public class Contact
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+
+    public override string ToString()
+    {
+        return $"ID: {Id}, Name: {FirstName} {LastName}, Phone: {PhoneNumber}, Email: {Email}";
+    }
+}
+// Класс ContactBook используется для управления списком контактов.
+public class ContactBook
+{
+    private List<Contact> contacts = new List<Contact>();
+    private int nextId = 1;
+
+    public void AddContact(string firstName, string lastName, string phoneNumber, string email)
+    {
+        var newContact = new Contact
+        {
+            Id = nextId++, // Auto-increment the ID
+            FirstName = firstName,
+            LastName = lastName,
+            PhoneNumber = phoneNumber,
+            Email = email
+        };
+
+        contacts.Add(newContact);
+        Console.WriteLine("Contact added successfully:\n{0}", newContact);
+    }
+
+    public IEnumerable<Contact> SearchContacts(string query)
+    {
+        return contacts.Where(c =>
+            c.FirstName.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0 ||
+            c.LastName.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0 ||
+            c.PhoneNumber.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0 ||
+            c.Email.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0);
+    }
+    // EditContact изменяет существующий контакт, найденный по ID.
+    public void EditContact(int id, string firstName, string lastName, string phoneNumber, string email)
+    {
+        var contact = contacts.FirstOrDefault(c => c.Id == id);
+        if (contact != null)
+        {
+            contact.FirstName = firstName;
+            contact.LastName = lastName;
+            contact.PhoneNumber = phoneNumber;
+            contact.Email = email;
+            Console.WriteLine("Contact updated successfully:\n{0}", contact);
+        }
+        else
+        {
+            Console.WriteLine($"Contact with ID {id} not found.");
+        }
+    }
+
+    public void DeleteContact(int id)
+    {
+        var contact = contacts.FirstOrDefault(c => c.Id == id);
+        if (contact != null)
+        {
+            contacts.Remove(contact);
+            Console.WriteLine($"Contact with ID {id} deleted successfully.");
+        }
+        else
+        {
+            Console.WriteLine($"Contact with ID {id} not found.");
+        }
+    }
+    // Отображает список всех контактов в книге.
+    public void ListAllContacts()
+    {
+        if (contacts.Any())
+        {
+            Console.WriteLine("Contacts:");
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine(contact);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No contacts found.");
+        }
+    }
+}
---
+++ examproject/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+class Program
+{// Главный класс для управления книгой контактов в консольном приложении.
+    static ContactBook contactBook = new ContactBook();
+
+    static void Main(string[] args)
+    {
+        while (true)
+        {// Выводим меню действий пользователю.
+            Console.WriteLine("\nChoose an option:");
+            Console.WriteLine("1: Add Contact");
+            Console.WriteLine("2: Search Contacts");
+            Console.WriteLine("3: Edit Contact");
+            Console.WriteLine("4: Delete Contact");
+            Console.WriteLine("5: List All Contacts");
+            Console.WriteLine("6: Exit");
+            Console.Write("Enter option: ");
+
+            string option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    AddContact();
+                    break;
+                case "2":
+                    SearchContacts();
+                    break;
+                case "3":
+                    EditContact();
+                    break;
+                case "4":
+                    DeleteContact();
+                    break;
+                case "5":
+                    ListAllContacts();
+                    break;
+                case "6":
+                    Console.WriteLine("Exiting...");
+                    return;
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");
+                    break;
+            }
+        }
+    }
+    // Собирает информацию от пользователя и добавляет новый контакт.
+    static void AddContact()
+    {
+        Console.Write("Enter first name: ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("Enter last name: ");
+        string lastName = Console.ReadLine();
+
+        Console.Write("Enter phone number: ");
+        string phoneNumber = Console.ReadLine();
+
+        Console.Write("Enter email: ");
+        string email = Console.ReadLine();
+        // Добавляем контакт используя введенные данные.
+        contactBook.AddContact(firstName, lastName, phoneNumber, email);
+    }
+    // Поиск контактов на основе запроса пользователя.
+    static void SearchContacts()
+    {
+        Console.Write("Enter search query: ");
+        string query = Console.ReadLine();
+        var foundContacts = contactBook.SearchContacts(query);
+
+        Console.WriteLine("Found contacts:");
+        foreach (var contact in foundContacts)
+        {
+            Console.WriteLine(contact);
+        }
+    }
+    // Редактирует существующий контакт, идентифицированный по ID.
+    static void EditContact()
+    {
+        Console.Write("Enter the ID of the contact to edit: ");
+        if (int.TryParse(Console.ReadLine(), out int id))
+        {
+            Console.Write("Enter new first name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Enter new last name: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Enter new phone number: ");
+            string phoneNumber = Console.ReadLine();
+
+            Console.Write("Enter new email: ");
+            string email = Console.ReadLine();
+
+            contactBook.EditContact(id, firstName, lastName, phoneNumber, email);
+        }
+        else
+        {
+            Console.WriteLine("Invalid ID format.");
+        }
+    }
+    // Удаляет контакт по ID, предоставленному пользователем.
+    static void DeleteContact()
+    {
+        Console.Write("Enter the ID of the contact to delete: ");
+        if (int.TryParse(Console.ReadLine(), out int id))
+        {
+            contactBook.DeleteContact(id);
+        }
+        else
+        {
+            Console.WriteLine("Invalid ID format.");
+        }
+    }
+
+    static void ListAllContacts()
+    {
+        contactBook.ListAllContacts();
+    }
+}
